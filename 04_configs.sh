def615049932797dcbaca702665b48a72ce1308a
Wesley Rodrigues <wesley.it@gmail.com>
2021-11-09 15:58:15 -0300

    Add samples of software config

--- 04_configs.sh
+++ 04_configs.sh
@@ -1,17 +1,42 @@
 ## Here you have some examples of configuration candies :D
 
-## Invoke the global bash completion
-#source /usr/share/bash-completion/bash_completion
+## Local bin inside $HOME must be executable
+export PATH="${PATH}:${HOME}/bin"
 
-## Include autocompletion for Amazon API commands
-#complete -C aws_completer aws
+## Invoke the global bash completion
+source /usr/share/bash-completion/bash_completion
 
 ## Include rbenv
 #export PATH="$HOME/.rbenv/bin:$PATH"
 #eval "$(rbenv init -)"
 
 ## Include Pyenv
-#export PYENV_ROOT="$HOME/.pyenv"
-#export PATH="$PYENV_ROOT/bin:$PATH"
-#eval "$(pyenv init -)"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+
+## Include Flutter
+export PATH="${PATH}:/opt/flutter/bin"
+
+## Android SDK
+export ANDROID_SDK_ROOT="/home/wesley/Android/Sdk"
+export PATH="${PATH}:${ANDROID_SDK_ROOT}/build-tools/31.0.0"
+export PATH="${PATH}:${ANDROID_SDK_ROOT}/platform-tools"
+export PATH="${PATH}:${ANDROID_SDK_ROOT}/tools"
+export PATH="${PATH}:${ANDROID_SDK_ROOT}/tools/bin"
+
+# Include Nodenv
+export PATH="$HOME/.nodenv/bin:$PATH"
+eval "$(nodenv init -)"
+
+## WPScan API Key
+export WPSCAN_API_KEY=''
+
+## GoLang
+export GOPATH="$HOME/.golang"
+
+## Rust
+export PATH="$PATH:$HOME/.cargo/bin"
+source $HOME/.cargo/env
 

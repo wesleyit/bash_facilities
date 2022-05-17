870458b5875510d063fb88d2e50df451fba9ebfb
Wesley Rodrigues <wesley.it@gmail.com>
2022-05-17 17:51:21 -0300

    Update 04_configs.sh

--- 04_configs.sh
+++ 04_configs.sh
@@ -11,32 +11,32 @@ source /usr/share/bash-completion/bash_completion
 #eval "$(rbenv init -)"
 
 ## Include Pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
+# export PYENV_ROOT="$HOME/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init --path)"
+# eval "$(pyenv init -)"
 
 ## Include Flutter
-export PATH="${PATH}:/opt/flutter/bin"
+# export PATH="${PATH}:/opt/flutter/bin"
 
 ## Android SDK
-export ANDROID_SDK_ROOT="/home/wesley/Android/Sdk"
-export PATH="${PATH}:${ANDROID_SDK_ROOT}/build-tools/31.0.0"
-export PATH="${PATH}:${ANDROID_SDK_ROOT}/platform-tools"
-export PATH="${PATH}:${ANDROID_SDK_ROOT}/tools"
-export PATH="${PATH}:${ANDROID_SDK_ROOT}/tools/bin"
+# export ANDROID_SDK_ROOT="/home/wesley/Android/Sdk"
+# export PATH="${PATH}:${ANDROID_SDK_ROOT}/build-tools/31.0.0"
+# export PATH="${PATH}:${ANDROID_SDK_ROOT}/platform-tools"
+# export PATH="${PATH}:${ANDROID_SDK_ROOT}/tools"
+# export PATH="${PATH}:${ANDROID_SDK_ROOT}/tools/bin"
 
 # Include Nodenv
-export PATH="$HOME/.nodenv/bin:$PATH"
-eval "$(nodenv init -)"
+# export PATH="$HOME/.nodenv/bin:$PATH"
+# eval "$(nodenv init -)"
 
 ## WPScan API Key
-export WPSCAN_API_KEY=''
+# export WPSCAN_API_KEY=''
 
 ## GoLang
-export GOPATH="$HOME/.golang"
+# export GOPATH="$HOME/.golang"
 
 ## Rust
-export PATH="$PATH:$HOME/.cargo/bin"
-source $HOME/.cargo/env
+# export PATH="$PATH:$HOME/.cargo/bin"
+# source $HOME/.cargo/env
 
